235cd01d1886a32490491eef632262b609796ee7
Kelvin Achi <kellywizy09@gmail.com>
2025-06-17 10:30:30 +0100

    footer completedt

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,38 @@
+import { FaXTwitter, FaFacebook, FaInstagram, FaTiktok } from "react-icons/fa6";
+
 const Footer = () => {
   return (
-    <div className="w-full">Footer</div>
+    <div className="grid grid-cols-2 lg:flex bg-[#221C1C] text-white p-4 sm:p-8 lg:p-16 max-w-[90vw] font-Itim m-auto mt-16 justify-between xl:gap-16">
+      <div className="w-full  flex flex-col items-center">
+        <h1 className="font-Irish text-xl md:text-2xl">Shop</h1>
+        <ul className="list-none mt-4 text-base sm:text-xl lg:text-2xl">
+          <li>Outerwears</li>
+          <li>Tops</li>
+          <li>Bottom</li>
+          <li>Footwears</li>
+          <li>Accessories</li>
+        </ul>
+      </div>
+      <div className="w-full  flex flex-col items-center row-start-2 col-start-1 col-end-3 m-auto mt-8 self-center">
+        <h1 className="font-Irish text-xl md:text-2xl">Follow us At</h1>
+        <div className="mt-6 text-white flex gap-2 sm:gap-4">
+          <FaXTwitter className="w-14 h-5 md:h-7"/>
+          <FaFacebook className="w-14 h-5 md:h-7"/>
+          <FaInstagram className="w-14 h-5 md:h-7"/>
+          <FaTiktok className="w-14 h-5 md:h-7"/>
+        </div>
+      </div>
+      <div className="w-full  flex flex-col items-center">
+        <h1 className="font-Irish text-xl md:text-2xl ">Lumine</h1>
+        <ul className="list-none text-base sm:text-xl lg:text-2xl mt-4">
+          <li>About Us</li>
+          <li>Become a Retailer</li>
+          <li>Contact Us</li>
+          <li>Blog</li>
+          <li>Terms & Conditions</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
--- app/components/home/newsletter.tsx
+++ app/components/home/newsletter.tsx
@@ -1,19 +1,19 @@
 const Newsletter = () => {
   return (
-    <div className="w-full min-h-24 sm:min-h-40 newsletter_gradient flex flex-col sm:flex-row justify-around items-center font-Jomolhari mt-14">
-     <h1 className="font-Jomolhari text-white text-base hidden sm:block">
+    <div className="w-full min-h-24  md:min-h-40 newsletter_gradient flex flex-col sm:flex-row justify-around items-center font-Jomolhari mt-14">
+     <h1 className="text-white text-base md:text-xl lg:text-2xl hidden sm:block">
         Sign up now to get alerts <br /> for new product drops
       </h1>
-      <h1 className="font-Jomolhari text-white text-[.9rem] sm:hidden ">
+      <h1 className="text-white text-[.9rem] sm:hidden ">
         Sign up now to get alerts for new product drops
       </h1>
-      <div className="flex items-center justify-between bg-white w-[15rem] sm:w-[20rem] md:w-[25rem] lg:w-[35rem] min-h-8 sm:min-h-16 rounded-[80px] border-[1px] border-[#e3e3e3]">
+      <div className="flex items-center justify-between bg-white w-[15rem] sm:w-[20rem] md:w-[25rem] sm:min-h-10 lg:w-[35rem] min-h-8 md:min-h-16 rounded-[80px] border-[1px] border-[#e3e3e3]">
         <input 
          type="email" 
          placeholder="Enter Email" 
          className="w-[20rem] ml-7 border-[none] outline-[none] text-[#616161] font-base text-xs sm:text-base"
         />
-        <button className="w-40 h-8 sm:h-16 rounded-[80px] bg-black text-white text-xs sm:text-base cursor-pointer ">Subscribe</button>
+        <button className="w-40 h-8 sm:h-10 md:h-16 rounded-[80px] bg-black text-white text-xs sm:text-base cursor-pointer ">Subscribe</button>
       </div>
     </div>
   )
