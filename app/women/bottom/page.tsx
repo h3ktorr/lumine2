a116b06ce0b062a328dc977440b2bd2de8b420bb
Kelvin Achi <kellywizy09@gmail.com>
2025-07-01 21:52:24 +0100

    womens bottom completed

--- app/women/bottom/page.tsx
+++ app/women/bottom/page.tsx
@@ -4,11 +4,11 @@ import Item from "@/app/components/item"
 const page = () => {
   return (
     <div className="mt-14 w-[90vw] m-auto flex flex-col items-center">
-     <div className="w-full h-[calc(100vh-56px)] women_accessories_bg bg-no-repeat bg-cover bg-center flex justify-center relative">
-      <h2 className="font-Jacques absolute text-[1.7rem] sm:text-4xl lg:text-5xl text-black top-[50%]">Accessories for you</h2>
+     <div className="w-full h-[calc(100vh-56px)] women_bottom_bg bg-no-repeat bg-cover bg-center flex justify-center relative">
+      <h2 className="font-Jacques absolute text-[1.7rem] sm:text-4xl lg:text-5xl text-white font-bold top-[50%]">Bottom for you</h2>
      </div>
      <div className="w-full overflow-hidden mt-14 flex flex-col">
-      <h1 className="font-Irish text-xl md:text-2xl cursor-pointer">Womens Accessories</h1>
+      <h1 className="font-Irish text-xl md:text-2xl cursor-pointer">Womens Bottom</h1>
       <div className="grid w-full grid-cols-2 md:grid-cols-3 mt-4 gap-4">
        {bestsellers.map(item=>{
          // console.log(item.image);
