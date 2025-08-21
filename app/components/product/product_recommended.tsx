99d3794802bb894b954782d0b4921ce3e92c24c8
Kelvin Achi <kellywizy09@gmail.com>
2025-08-21 05:44:12 +0100

    recommended product refactored

--- app/components/product/product_recommended.tsx
+++ app/components/product/product_recommended.tsx
@@ -1,27 +1,50 @@
-import bestsellers from "../bestsellers"
-import Item from "../item"
+import { wixClientServer } from "@/app/lib/wixClientServer";
+import Item from "../item";
+import { products } from "@wix/stores";
+import { Suspense } from "react";
 
-const Product_recommended = () => {
+const Product_recommended = async ({ collectionIds }: { collectionIds: string[] }) => {
+  const wixClient = await wixClientServer();
+
+  const res = await wixClient.products
+    .queryProducts()
+    .eq("collectionIds", collectionIds[0])
+    .find();
+
+  const allIds = res.items.map((p: products.Product) => p._id!).filter(Boolean);
+
+  const randomIds = allIds.sort(() => 0.5 - Math.random()).slice(0, 3);
+
+  const productResponses = await Promise.all(
+    randomIds.map((id) => wixClient.products.getProduct(id))
+  );
+
+  const productsData: products.Product[] = productResponses
+    .map((res) => res.product)
+    .filter((p): p is products.Product => Boolean(p));
+
   return (
     <div className="w-full overflow-hidden mt-12 flex flex-col">
-     <h1 className="font-Irish text-xl">Recommended for you</h1>
-     <div className="flex gap-5 mt-4 recommended-items">
-     {bestsellers.slice(0,3).map(item=>{
-        const images = [...item.image]
-        return (
-          <Item 
-            key={item.id}
-            id={item.id}
-            name={item.name}
-            image={images}
-            price={item.price}
-          />
-        )
-      })}
-     </div>
-     <button className="bg-[#E8E8E6] w-28 h-8 rounded-[10px] self-end mt-4 font-Itim text-base text-black">View More</button>
+      <h1 className="font-Irish text-xl">Recommended for you</h1>
+      <div className="flex gap-5 mt-4 recommended-items">
+        {productsData.map((product) => {
+          const images = product.media?.items?.map((item) => item.image?.url || "");
+          return (
+            <Suspense fallback={"loading"} key={product._id}>
+              <Item
+                product={product}
+                id={product._id!}
+                name={product.name || ""}
+                image={images!}
+                price={product.priceData?.price || 0}
+                slug={product.slug!}
+              />
+            </Suspense>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Product_recommended
+export default Product_recommended;
